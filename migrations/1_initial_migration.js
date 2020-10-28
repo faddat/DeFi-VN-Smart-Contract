af8fa5843bd595e3283690a522a68895b65778da
tungdt <tungdt@edsolabs.com>
2020-10-28 10:02:43 +0700

    Add migration and build

---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,18 @@
+const DFY = artifacts.require("DFY");
+const BigNumber = require('bignumber.js');
+
+module.exports = (deployer, network, accounts) => {
+  return deployer.then(async () => {
+
+    await deployer.deploy(
+      DFY,
+      "Decentralized Finance Token",
+      "DFI",
+      18,
+      new BigNumber(1e30),
+      new BigNumber(0),
+      false
+    );
+    console.log(`[ADDRESS] DFY contracts: ${DFY.address}`);
+  });
+};
---
+++ truffle-config.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+require("chai/register-should");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+const solcStable = {
+  version: "0.7.2",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  }
+};
+
+const solcNightly = {
+  version: "nightly",
+  docker: true
+};
+
+const useSolcNightly = process.env.SOLC_NIGHTLY === "true";
+
+module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // eslint-disable-line camelcase
+    },
+    coverage: {
+      host: "localhost",
+      network_id: "*", // eslint-disable-line camelcase
+      port: 8555,
+      gas: 0xfffffffffff,
+      gasPrice: 0x01
+    },
+    bscTest: {
+      provider: () => {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          "https://data-seed-prebsc-1-s1.binance.org:8545"
+        );
+      },
+      from: process.env.WALLET_ADDRESS,
+      gas: 8812388,
+      // gasPrice: 8000000000,
+      network_id: 97
+    }
+  },
+  compilers: {
+    solc: useSolcNightly ? solcNightly : solcStable
+  },
+  plugins: ["solidity-coverage"]
+};
