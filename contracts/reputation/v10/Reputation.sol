f1bd14bde4fe980b17150225acd4c356c426a81d
Lê Hoàng Anh <anhlh@blockchaindeveloper.asia>
2021-09-28 19:09:03 +0700

    Changed to Reputation to allow multiple contract caller.

--- contracts/reputation/v10/Reputation.sol
+++ contracts/reputation/v10/Reputation.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-// Will be replaced by DFY-AccessControl when it's merged or later phases.
+// Will be replaced by DFY-AccessControl when it's merged.
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
@@ -34,9 +34,25 @@ contract Reputation is
 
     address _contractCaller;
 
-    mapping(ReasonType => int8) _rewardByReason;
+    // Reason for Reputation point adjustment
+    /**
+    * @dev Reputation points in correspondence with ReasonType 
+    * LD_CREATE_PACKAGE     : +3
+    * LD_CANCEL_PACKAGE     : -3
+    * LD_REOPEN_PACKAGE     : +3
+    * LD_GENERATE_CONTRACT  : +1
+    * LD_CREATE_OFFER       : +2
+    * LD_CANCEL_OFFER       : -2
+    * BR_CREATE_COLLATERAL  : +3
+    * BR_CANCEL_COLLATERAL  : -3
+    * BR_ONTIME_PAYMENT     : +1
+    * BR_LATE_PAYMENT       : -1
+    * BR_ACCEPT_OFFER       : +1
+    * BR_CONTRACT_COMPLETE  : +5
+    * BR_CONTRACT_DEFAULTED : -5
+    */
 
-    mapping(address => bool) whitelistedContractCaller;
+    mapping(ReasonType => int8) _rewardByReason; 
 
     event ReputationPointRewarded(address _user, uint256 _points, ReasonType _reasonType);
     event ReputationPointReduced(address _user, uint256 _points, ReasonType _reasonType);
@@ -51,61 +67,26 @@ contract Reputation is
         _initializeRewardByReason();
     }
 
-    // Reason for Reputation point adjustment
-    /**
-    * @dev Reputation points in correspondence with ReasonType 
-    * LD_CREATE_PACKAGE         : +3    (0)
-    * LD_CANCEL_PACKAGE         : -3    (1)
-    * LD_REOPEN_PACKAGE         : +3    (2)
-    * LD_GENERATE_CONTRACT      : +1    (3)
-    * LD_CREATE_OFFER           : +2    (4)
-    * LD_CANCEL_OFFER           : -2    (5)
-    * LD_ACCEPT_OFFER           : +1    (6)
-    * BR_CREATE_COLLATERAL      : +3    (7)
-    * BR_CANCEL_COLLATERAL      : -3    (8)
-    * BR_ONTIME_PAYMENT         : +1    (9)
-    * BR_LATE_PAYMENT           : -1    (10)
-    * BR_ACCEPT_OFFER           : +1    (11)
-    * BR_CONTRACT_COMPLETE      : +5    (12)
-    * BR_CONTRACT_DEFAULTED     : -5    (13)
-    * LD_REVIEWED_BY_BORROWER_1 : +1    (14)
-    * LD_REVIEWED_BY_BORROWER_2 : +2    (15)
-    * LD_REVIEWED_BY_BORROWER_3 : +3    (16)
-    * LD_REVIEWED_BY_BORROWER_4 : +4    (17)
-    * LD_REVIEWED_BY_BORROWER_5 : +5    (18)
-    * LD_KYC                    : +5    (19)
-    * BR_REVIEWED_BY_LENDER_1   : +1    (20)
-    * BR_REVIEWED_BY_LENDER_2   : +2    (21)
-    * BR_REVIEWED_BY_LENDER_3   : +3    (22)
-    * BR_REVIEWED_BY_LENDER_4   : +4    (23)
-    * BR_REVIEWED_BY_LENDER_5   : +5    (24)
-    * BR_KYC                    : +5    (25)
-    */
     function _initializeRewardByReason() internal virtual {
-        _rewardByReason[ReasonType.LD_CREATE_PACKAGE]    =  3;  // index: 0
-        _rewardByReason[ReasonType.LD_CANCEL_PACKAGE]    = -3;  // index: 1
-        _rewardByReason[ReasonType.LD_REOPEN_PACKAGE]    =  3;  // index: 2
-        _rewardByReason[ReasonType.LD_GENERATE_CONTRACT] =  1;  // index: 3
-        _rewardByReason[ReasonType.LD_CREATE_OFFER]      =  2;  // index: 4
-        _rewardByReason[ReasonType.LD_CANCEL_OFFER]      = -2;  // index: 5
-        _rewardByReason[ReasonType.LD_ACCEPT_OFFER]      =  1;  // index: 6
-        _rewardByReason[ReasonType.BR_CREATE_COLLATERAL] =  3;  // index: 7
-        _rewardByReason[ReasonType.BR_CANCEL_COLLATERAL] = -3;  // index: 8
-        _rewardByReason[ReasonType.BR_ONTIME_PAYMENT]    =  1;  // index: 9
-        _rewardByReason[ReasonType.BR_LATE_PAYMENT]      = -1;  // index: 10
-        _rewardByReason[ReasonType.BR_ACCEPT_OFFER]      =  1;  // index: 11
-        _rewardByReason[ReasonType.BR_CONTRACT_COMPLETE] =  5;  // index: 12
-        _rewardByReason[ReasonType.BR_CONTRACT_DEFAULTED]= -5;  // index: 13
-    }
-
-    function initializeRewardByReason() external onlyRole(DEFAULT_ADMIN_ROLE) {
-        _initializeRewardByReason();
+        _rewardByReason[ReasonType.LD_CREATE_PACKAGE]    =  3;
+        _rewardByReason[ReasonType.LD_CANCEL_PACKAGE]    = -3;
+        _rewardByReason[ReasonType.LD_REOPEN_PACKAGE]    =  3;
+        _rewardByReason[ReasonType.LD_GENERATE_CONTRACT] =  1;
+        _rewardByReason[ReasonType.LD_CREATE_OFFER]      =  2;
+        _rewardByReason[ReasonType.LD_CANCEL_OFFER]      = -2;
+        _rewardByReason[ReasonType.BR_CREATE_COLLATERAL] =  3;
+        _rewardByReason[ReasonType.BR_CANCEL_COLLATERAL] = -3;
+        _rewardByReason[ReasonType.BR_ONTIME_PAYMENT]    =  1;
+        _rewardByReason[ReasonType.BR_LATE_PAYMENT]      = -1;
+        _rewardByReason[ReasonType.BR_ACCEPT_OFFER]      =  1;
+        _rewardByReason[ReasonType.BR_CONTRACT_COMPLETE] =  5;
+        _rewardByReason[ReasonType.BR_CONTRACT_DEFAULTED]= -5;
     }
 
     function _authorizeUpgrade(address) internal override onlyRole(DEFAULT_ADMIN_ROLE) {}
 
     function version() public virtual pure returns (string memory) {
-        return "1.0.2";
+        return "1.0";
     }
 
     modifier isNotZeroAddress(address _to) {
@@ -118,24 +99,13 @@ contract Reputation is
         _;
     }
 
-    // TODO: Will be removed later
     modifier onlyContractCaller() {
         require(_contractCaller == _msgSender(), "DFY: Calling Reputation adjustment from a non-contract address");
         _;
     }
 
-    modifier onlyWhitelistedContractCaller(address _from) {
-        // Caller must be a contract
-        require(_from.isContract(), "DFY: Calling Reputation adjustment from a non-contract address");
-
-        // Caller must be whitelisted
-        require(whitelistedContractCaller[_from] == true, "DFY: Caller is not allowed");
-        _;
-    }
-
     /**
     * @dev Get the address of the host contract
-    * TODO: Will be removed later
     */
     function getContractCaller() external virtual view returns (address) {
         return _contractCaller;
@@ -143,41 +113,14 @@ contract Reputation is
     
     /**
     * @dev Set the host contract address that only allowed to call functions from this contract
-    * TODO: Will be removed later
     */
     function setContractCaller(address _caller) external onlyRole(DEFAULT_ADMIN_ROLE) {
         _setContractCaller(_caller);
     }
 
-    // TODO: Will be removed later
     function _setContractCaller(address _caller) internal {
         require(_caller.isContract(), "DFY: Setting reputation contract caller to a non-contract address");
         _contractCaller = _caller;
-    }
-
-    /** 
-    * @dev Add a contract address that use Reputation to whitelist
-    * @param _caller is the contract address being whitelisted=
-    */
-    function addWhitelistedContractCaller(address _caller) external onlyRole(DEFAULT_ADMIN_ROLE) {
-        require(_caller.isContract(), "DFY: Setting reputation contract caller to a non-contract address");
-        whitelistedContractCaller[_caller] = true;
-    }
-
-    /** 
-    * @dev remove a contract address from whitelist
-    * @param _caller is the contract address being removed
-    */
-    function removeWhitelistedContractCaller(address _caller) external onlyRole(DEFAULT_ADMIN_ROLE) {
-        delete whitelistedContractCaller[_caller];
-    }
-
-    /** 
-    * @dev check if an address is whitelisted
-    * @param _contract is the address being verified
-    */
-    function isWhitelistedContractCaller(address _contract) external view returns (bool) {
-        return whitelistedContractCaller[_contract];
     }
 
     /**
@@ -186,6 +129,7 @@ contract Reputation is
     function getReputationScore(address _address) external virtual override view returns(uint32) {
         return _reputationScore[_address];
     }
+
 
     /**
     * @dev Return the absolute value of a signed integer
@@ -205,7 +149,7 @@ contract Reputation is
         address _user, 
         ReasonType _reasonType) 
         external override
-        whenNotPaused isNotZeroAddress(_user) onlyEOA(_user) onlyWhitelistedContractCaller(_msgSender())
+        whenNotPaused isNotZeroAddress(_user) onlyEOA(_user) onlyContractCaller
     {
         int8 pointsByReason     = _rewardByReason[_reasonType];
         uint256 points          = abs(pointsByReason);
